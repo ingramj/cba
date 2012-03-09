2410fb2d228a15f9162ee000c59be19a00586997
Jim Ingram <ingramj@gmail.com>
2012-03-08 19:30:30 -0600

    Removed FFI, at least until the api is settled.

--- ffi/python/cba.py
+++
@@ -1,6 +0,0 @@
-from ctypes import *
-
-_libcba = cdll.LoadLibrary('libcba.so')
-
-def cba_true():
-    return _libcba.cba_true()
--- ffi/ruby/cba.rb
+++
@@ -1,7 +0,0 @@
-require 'ffi'
-
-module CBitArray
-  extend FFI::Library
-  ffi_lib 'cba'
-  attach_function 'cba_true', [], :int
-end
